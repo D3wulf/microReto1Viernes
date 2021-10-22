986cc896436a196450fa5c5b605910ce2428f42c
Miguel Angel <vivanlasfallas@hotmail.com>
2021-10-22 10:16:19 +0200

    primer commit

---
+++ src/main/java/microservicioViernesReto/reto1Viernes/Reto1ViernesApplication.java
@@ -0,0 +1,13 @@
+package microservicioViernesReto.reto1Viernes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto1ViernesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto1ViernesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/microservicioViernesReto/reto1Viernes/Reto1ViernesApplicationTests.java
@@ -0,0 +1,13 @@
+package microservicioViernesReto.reto1Viernes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto1ViernesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
