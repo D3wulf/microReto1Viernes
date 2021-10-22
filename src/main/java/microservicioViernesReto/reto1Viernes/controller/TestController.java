550d4d5a1551ee2d27165db2402cde2208ec3505
Miguel Angel <vivanlasfallas@hotmail.com>
2021-10-22 12:06:42 +0200

    añadimos actuator y probamos el refresh

--- src/main/java/microservicioViernesReto/reto1Viernes/controller/TestController.java
+++ src/main/java/microservicioViernesReto/reto1Viernes/controller/TestController.java
@@ -1,9 +1,11 @@
 package microservicioViernesReto.reto1Viernes.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class TestController {
 	
